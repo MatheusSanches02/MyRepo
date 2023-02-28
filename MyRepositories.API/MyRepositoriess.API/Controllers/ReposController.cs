60a32d84ccde3f3ac492006684ba771107a0d0eb
MatheusSanches02 <rm87426@fiap.com.br>
2023-02-27 23:05:34 -0300

    get repositórios

---
+++ MyRepositories.API/MyRepositoriess.API/Controllers/ReposController.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using MyRepositoriess.API.Models;
+using System.Data.SqlClient;
+
+namespace MyRepositoriess.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReposController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public ReposController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Repos>>> GetAllRepositories()
+        {
+            using var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+            var repos = await connection.QueryAsync<Repos>("select * from repos");
+            return Ok(repos);
+        }
+    }
+}
---
+++ MyRepositories.API/MyRepositoriess.API/Models/Repos.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyRepositoriess.API.Models
+{
+    public class Repos
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public string Language { get; set; } = String.Empty;
+        public DateTime LastUpdate { get; set; }
+        public string? RepositorieOwner { get; set; } 
+    }
+}
